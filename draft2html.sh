3d2cc2942215b680731140d65c76c5c64021f665
Kimiaki Kuno <knokmki612@gmail.com>
2015-04-29 22:41:48 +0900

    tmpファイルの削除と画像が見つからなかった際のexitを追加

--- draft2html.sh
+++ draft2html.sh
@@ -5,6 +5,7 @@ tmp=./tmp
 for file in `sed -e '/^\(.*[.png|.jpeg|.jpg]\)$/!d' $tmp | uniq`; do
 	if [ ! -f $file ]; then
 		echo "$file: No such image in this directory."
+		exit 1
 	fi
 done
 
@@ -12,4 +13,5 @@ cat $draft | sed '1,3d' | tr -d '\r' > $tmp
 sed -e 's/^$/<br>/g' -e 's/^\([^<].*\)/<p>\1<\/p>/g' $tmp
 #-e 's/^\(.*[.png|.jpeg|.jpg]\)$/<p class="image"><a href="\1"><img class="landscape" src="\1" alt=""><\/a><\/p>/g' 
 
+rm $tmp
 exit 0
