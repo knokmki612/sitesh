01be2bbed8ca5ecfa5c7d79a93978a086a66f9eb
Kimiaki Kuno <knokmki612@gmail.com>
2015-04-29 22:40:12 +0900

    手軽な手書きフォーマットからhtmlに変換するdraft2html.shを書き始めた。pタグやbrタグの置き換えは可能。画像ファイルのリンク切れチェックの実装までやった

---
+++ draft2html.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+draft=$1
+tmp=./tmp
+
+for file in `sed -e '/^\(.*[.png|.jpeg|.jpg]\)$/!d' $tmp | uniq`; do
+	if [ ! -f $file ]; then
+		echo "$file: No such image in this directory."
+	fi
+done
+
+cat $draft | sed '1,3d' | tr -d '\r' > $tmp
+sed -e 's/^$/<br>/g' -e 's/^\([^<].*\)/<p>\1<\/p>/g' $tmp
+#-e 's/^\(.*[.png|.jpeg|.jpg]\)$/<p class="image"><a href="\1"><img class="landscape" src="\1" alt=""><\/a><\/p>/g' 
+
+exit 0
