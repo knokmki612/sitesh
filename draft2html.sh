422a89c59693f9ef7829455b62823b1055d1c0b3
Kimiaki Kuno <knokmki612@gmail.com>
2015-05-02 15:16:20 +0900

    画像タグにaltを埋め込めるようにした 画像の縦向き横向き判定を加えた 画像をまとまったpタグで囲うようにした 引数がないときにメッセージを出すようにした

--- draft2html.sh
+++ draft2html.sh
@@ -1,17 +1,72 @@
 #!/bin/sh
 draft=$1
 tmp=./tmp
-cat $draft | sed '1,3d' | tr -d '\r' > $tmp
 
-for file in `sed -e '/^\(.*[.png|.jpeg|.jpg]\)$/!d' $tmp | sort -u`; do
-	if [ ! -f $file ]; then
-		echo "$file: No such image in this directory."
+if [ $# -ne 1 ]; then
+	echo 'usage: draft2html [`date +%Y%M%d`-draft]'
+	exit 1
+fi
+
+# htmlタグに対応するための一時ファイル
+cat $draft    |
+sed '1,3d'    |
+grep -ve '^#' |
+tr -d '\r' > $tmp
+
+# スペースを含んだaltメッセージに対応するため、スペース区切りを無効化
+IFS_BACKUP=$IFS
+IFS=$'\n'
+
+# 上から順番に画像タグを検出
+while true; do
+	image=`
+		grep -n -m 1 -e '\(.*.png\|.*.jpeg\|.*.jpg\):' $tmp`
+	if [ $? -ne 0 ]; then
+		break
+	fi
+	linenum=`
+		echo $image | cut -d ':' -f 1`
+	filename=`
+		echo $image | cut -d ':' -f 2`
+	alt=`
+		echo $image | cut -d ':' -f 3`
+	if [ ! -f $filename ]; then
+		echo "$filename: No such image in this directory."
 		exit 1
+	fi
+
+	width=`
+		identify $filename | cut -d ' ' -f 3 | cut -d 'x' -f 1`
+	height=`
+		identify $filename | cut -d ' ' -f 3 | cut -d 'x' -f 2`
+	if [ $width -ge $height ]; then
+		orientation='landscape'
+	else
+		orientation='portrait'
+	fi
+
+	# 連続して画像タグがある場合に、pタグをまとめる
+	if \
+		cat $tmp                     |
+		head -n $(expr $linenum - 2) |
+		tail -n 1                    |
+		grep -sq -e '<img class="\(landscape\|portrait\)"'; then
+		sed -i \
+			-e `expr $linenum - 1`'d' \
+			-e $linenum'a<\/p>' \
+			-e $linenum'c<a href="'$filename'"><img class="'$orientation'" src="'$filename'" alt="'$alt'"><\/a>' $tmp
+	else
+		sed -i \
+			-e $linenum'i<p class="image">' \
+			-e $linenum'a<\/p>' \
+			-e $linenum'c<a href="'$filename'"><img class="'$orientation'" src="'$filename'" alt="'$alt'"><\/a>' $tmp
 	fi
 done
 
+IFS=$IFS_BACKUP
+
+# brタグ、pタグを入れる
 sed -e 's/^$/<br>/g' -e 's/^\([^<].*\)/<p>\1<\/p>/g' $tmp
-#-e 's/^\(.*[.png|.jpeg|.jpg]\)$/<p class="image"><a href="\1"><img class="landscape" src="\1" alt=""><\/a><\/p>/g' 
 
 rm $tmp
 exit 0
