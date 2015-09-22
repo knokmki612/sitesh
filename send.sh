19f505269e2ba30c4ab2aa428dde8e5bd711dd15
Kimiaki Kuno <knokmki612@gmail.com>
2015-09-22 23:57:46 +0900

    複数の記事でパスワード入力を共通化するためにssh-agentを使うようにした

--- send.sh
+++ send.sh
@@ -15,7 +15,10 @@ read select_num
 select_num=$(echo $select_num | sed 's/\([^ ]*\)/-e \1p/g')
 echo ""
 
+eval $(ssh-agent)
+ssh-add ~/.ssh/id_rsa
 echo "$list" | sed -n $select_num | xargs -I @ rsync -auvz --delete -e ssh $(pwd)/@ $SITE_DOMAIN:$SITE_ABSOLUTE_PATH${SITE_POSTS_DIR}
+eval $(ssh-agent -k)
 
 echo ""
 exit 0
