6829d900bd3637153e3092606da0d7c6cfaa3637
Kimiaki Kuno <knokmki612@gmail.com>
2015-10-13 17:12:03 +0900

    記事を送った後、キャッシュ生成スクリプトを実行するようにした

--- send.sh
+++ send.sh
@@ -18,6 +18,7 @@ echo ""
 eval $(ssh-agent)
 ssh-add ~/.ssh/id_rsa
 echo "$list" | sed -n $select_num | xargs -I @ rsync -auvz --delete -e ssh $(pwd)/@ $SITE_DOMAIN:$SITE_ABSOLUTE_PATH${SITE_POSTS_DIR}
+ssh $SITE_DOMAIN "cd $SITE_ABSOLUTE_PATH; ./make_archives.dash; ./make_labels.dash; ./make_search_cache.dash"
 eval $(ssh-agent -k)
 
 echo ""
