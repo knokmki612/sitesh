9ada48d36975b07cfa4cdcc2a984568f0628c355
Kimiaki Kuno <knokmki612@gmail.com>
2015-11-27 01:19:36 +0900

    template-article.html.shの削除、apache-2.4.conf.example.shの作成

---
+++ share/site/templates/apache-2.4.conf.example.sh
@@ -0,0 +1,46 @@
+cat << +
+<VirtualHost *:80>
+	ServerAdmin webmaster@localhost
+	ServerName $SITE_DOMAIN:80
+
+	DocumentRoot $SITE_ABSOLUTE_PATH
+
+	<Directory />
+		Options FollowSymLinks
+		AllowOverride None
+	</Directory>
+
+	<Directory $SITE_ABSOLUTE_PATH>
+		Options ExecCGI FollowSymLinks
+		AllowOverride None
+
+		RewriteEngine on
+		RewriteCond %{REQUEST_URI} ^/\$
+		RewriteCond %{QUERY_STRING} ^\$
+		RewriteRule ^ ?archive=latest [L]
+		RewriteCond %{REQUEST_URI} ^/post/([^/]+)/?\$
+		RewriteRule ^ ?post=%1 [L]
+
+		ExpiresActive on
+		ExpiresByType image/x-icon \"access plus 1 month\"
+		ExpiresByType image/jpeg \"access plus 1 month\"
+		ExpiresByType image/png \"access plus 1 month\"
+		ExpiresByType font/truetype \"access plus 1 month\"
+		<FilesMatch \"normalize.css\">
+			ExpiresDefault \"access plus 1 year\"
+		</FilesMatch>
+	</Directory>
+
+	<FilesMatch \"\\\.cgi\$\">
+		SetHandler cgi-script
+	</FilesMatch>
+
+	ErrorLog \\\${APACHE_LOG_DIR}/error.log
+
+	# Possible values include: debug, info, notice, warn, error, crit,
+	# alert, emerg.
+	LogLevel warn
+
+	CustomLog \\\${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>
++
