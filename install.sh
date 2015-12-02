c2ef283f079f32ec967de16868628e48f4f8a340
Kimiaki Kuno <knokmki612@gmail.com>
2015-12-02 14:59:19 +0900

    install.shのインストール先は/usr/localに固定した

--- install.sh
+++ install.sh
@@ -1,13 +1,3 @@
 #!/bin/sh
-if [ $# -eq 0 ]; then
-	echo "usage: install.sh {usr|usr/local}"
-fi
-
-case $1 in
-	usr)
-		sudo cp -r bin share /usr
-		;;
-	usr/local)
-		sudo cp -r bin share /usr/local
-		;;
-esac
+cd $(dirname $0)
+sudo cp -r bin share /usr/local
